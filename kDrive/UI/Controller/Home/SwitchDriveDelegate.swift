d2bd659d92aa71e881c424c2a56b9486071e11f7
Florentin Bekier <florentin.bekier@infomaniak.com>
2021-04-27 17:07:58 +0200

    Fix warning

--- kDrive/UI/Controller/Home/SwitchDriveDelegate.swift
+++ kDrive/UI/Controller/Home/SwitchDriveDelegate.swift
@@ -19,6 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import Foundation
 import kDriveCore
 
-protocol SwitchDriveDelegate: class {
+protocol SwitchDriveDelegate: AnyObject {
     func didSwitchDrive(newDrive: Drive)
 }
