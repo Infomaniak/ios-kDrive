746d216399ac5693019f94dce589d47180897423
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2021-04-29 22:20:17 +0200

    TestFlight build 48 - Beta 13 build 🚀
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- Project.swift
+++ Project.swift
@@ -21,7 +21,7 @@ import ProjectDescription
 let baseSettings: [String: SettingValue] = [
     "CODE_SIGN_STYLE": "Automatic",
     "DEVELOPMENT_TEAM": "864VDCS2QY",
-    "CURRENT_PROJECT_VERSION": "47",
+    "CURRENT_PROJECT_VERSION": "48",
     "MARKETING_VERSION": "4.0.0"
 ]
 
