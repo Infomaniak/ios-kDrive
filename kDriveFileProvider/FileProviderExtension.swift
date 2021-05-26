4505e79de5641e6909486ee9f36da452d6532ba0
Florentin Bekier <florentin.bekier@infomaniak.com>
2021-05-26 16:01:57 +0200

    Try prevent calling startProvidingItemAtURL twice
    Signed-off-by: Florentin Bekier <florentin.bekier@infomaniak.com>

--- kDriveFileProvider/FileProviderExtension.swift
+++ kDriveFileProvider/FileProviderExtension.swift
@@ -140,7 +140,8 @@ class FileProviderExtension: NSFileProviderExtension {
     }
 
     override func startProvidingItem(at url: URL, completionHandler: @escaping (Error?) -> Void) {
-        guard let fileId = FileProviderItem.identifier(for: url, domain: domain)?.toFileId() else {
+        guard let fileId = FileProviderItem.identifier(for: url, domain: domain)?.toFileId(),
+            let file = driveFileManager.getCachedFile(id: fileId) else {
             if FileManager.default.fileExists(atPath: url.path) {
                 completionHandler(nil)
             } else {
@@ -149,26 +150,24 @@ class FileProviderExtension: NSFileProviderExtension {
             return
         }
 
-        if let file = driveFileManager.getCachedFile(id: fileId) {
-            let item = FileProviderItem(file: file, domain: domain)
+        let item = FileProviderItem(file: file, domain: domain)
 
-            if !FileManager.default.fileExists(atPath: item.storageUrl.path) {
-                //File is not in the file provider we have to download/copy it
-                downloadRemoteFile(file: file, for: item, completion: completionHandler)
-            } else if fileStorageIsCurrent(item: item, file: file) {
-                //File is in the file provider and is the same, nothing to do...
-                manager.signalEnumerator(for: item.parentItemIdentifier) { _ in }
-                completionHandler(nil)
-            } else {
-                //File from file provider has changes not synced with cloud, we have to upload the local file
-                if !FileManager.default.contentsEqual(atPath: item.storageUrl.path, andPath: file.localUrl.path) {
-                    //This is not optimal because we wait for the item to be uploaded before downloading the remote
-                    backgroundUploadItem(item) {
-                        self.downloadRemoteFile(file: file, for: item, completion: completionHandler)
-                    }
-                } else {
-                    downloadRemoteFile(file: file, for: item, completion: completionHandler)
+        if !FileManager.default.fileExists(atPath: item.storageUrl.path) {
+            // File is not in the file provider we have to download/copy it
+            downloadRemoteFile(file: file, for: item, completion: completionHandler)
+        } else if fileStorageIsCurrent(item: item, file: file) {
+            // File is in the file provider and is the same, nothing to do...
+            manager.signalEnumerator(for: item.parentItemIdentifier) { _ in }
+            completionHandler(nil)
+        } else {
+            // File from file provider has changes not synced with cloud, we have to upload the local file
+            if !FileManager.default.contentsEqual(atPath: item.storageUrl.path, andPath: file.localUrl.path) {
+                // This is not optimal because we wait for the item to be uploaded before downloading the remote
+                backgroundUploadItem(item) {
+                    self.downloadRemoteFile(file: file, for: item, completion: completionHandler)
                 }
+            } else {
+                downloadRemoteFile(file: file, for: item, completion: completionHandler)
             }
         }
     }
@@ -185,6 +184,11 @@ class FileProviderExtension: NSFileProviderExtension {
 
     private func downloadRemoteFile(file: File, for item: FileProviderItem, completion: @escaping (Error?) -> ()) {
         if file.isLocalVersionOlderThanRemote() {
+            // Prevent observing file multiple times
+            guard DownloadQueue.instance.operationsInQueue[file.id] == nil else {
+                completion(nil)
+                return
+            }
             DownloadQueue.instance.observeFileDownloaded(self, fileId: file.id) { (fileId, error) in
                 if error != nil {
                     self.manager.signalEnumerator(for: item.parentItemIdentifier) { _ in }
