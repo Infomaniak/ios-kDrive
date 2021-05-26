d9e6f23b0b5ebe9a27eae90fed6e75ce736470e0
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2021-05-26 14:51:32 +0200

    Photo list display months
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- kDrive/UI/Controller/Menu/PhotoListViewController.swift
+++ kDrive/UI/Controller/Menu/PhotoListViewController.swift
@@ -24,14 +24,34 @@ class PhotoListViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
 
-    private var files = [File]()
+    private let dateFormatter = DateFormatter()
+    private class YearMonthPictures {
+        let referenceDate: Date
+        let year: Int
+        let month: Int
+        var pictures: [File]
+
+        init(referenceDate: Date) {
+            self.referenceDate = referenceDate
+            let components = Calendar.current.dateComponents([.year, .month], from: referenceDate)
+            self.year = components.year!
+            self.month = components.month!
+            self.pictures = [File]()
+        }
+    }
+
+    private var pictureForYearMonth = [YearMonthPictures]()
+    private var pictures = [File]()
     private var page = 1
     private var hasNextPage = true
-    private var isLoading = true
+    private var isLoading = true {
+        didSet {
+            collectionView?.collectionViewLayout.invalidateLayout()
+        }
+    }
     private var isLargeTitle = true {
         didSet { updateNavBarMode() }
     }
-    private var footerView: UICollectionReusableView?
     private lazy var filePresenter = FilePresenter(viewController: self, floatingPanelViewController: floatingPanelViewController)
     private var floatingPanelViewController: DriveFloatingPanelController?
 
@@ -44,6 +64,7 @@ class PhotoListViewController: UIViewController {
     }
 
     private let footerIdentifier = "LoadingFooterView"
+    private let headerIdentifier = "PhotoSectionHeaderView"
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return isLargeTitle ? .default : .lightContent
@@ -51,6 +72,19 @@ class PhotoListViewController: UIViewController {
 
     var driveFileManager: DriveFileManager!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dateFormatter.dateFormat = "MMMM YYYY"
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(cellView: HomeLastPicCollectionViewCell.self)
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: footerIdentifier)
+        collectionView.register(UINib(nibName: "PhotoSectionHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
+        (collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.sectionHeadersPinToVisibleBounds = true
+        fetchNextPage()
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
@@ -66,24 +100,11 @@ class PhotoListViewController: UIViewController {
             updateNavBarMode()
         }
         navigationController?.navigationBar.isTranslucent = true
-
-        updateNavBarMode()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.navigationBar.tintColor = nil
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(cellView: HomeLastPicCollectionViewCell.self)
-        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: footerIdentifier)
-
-        fetchNextPage()
     }
 
     func generateGradient() -> UIImage? {
@@ -108,26 +129,52 @@ class PhotoListViewController: UIViewController {
 
     func forceRefresh() {
         page = 1
-        files = []
+        pictures = []
+        pictureForYearMonth = []
         fetchNextPage()
     }
 
     func fetchNextPage() {
-        footerView?.isHidden = false
         isLoading = true
         driveFileManager?.apiFetcher.getLastPictures(page: page) { (response, error) in
-            if let data = response?.data {
-                let lastIndex = self.files.count
-                self.files += data
-                self.collectionView.insertItems(at: (lastIndex..<self.files.count).map { IndexPath(row: $0, section: 0) })
+            if let fetchedPictures = response?.data {
+
+                self.collectionView.performBatchUpdates {
+                    for picture in fetchedPictures {
+                        let components = Calendar.current.dateComponents([.year, .month], from: picture.lastModifiedDate)
+
+                        var currentSectionIndex: Int!
+                        var currentYearMonth: YearMonthPictures!
+                        let lastYearMonth = self.pictureForYearMonth.last
+                        let currentPictureMonth = components.month!
+                        let currentPictureYear = components.year!
+                        if lastYearMonth?.month == currentPictureMonth && lastYearMonth?.year == currentPictureYear {
+                            currentYearMonth = lastYearMonth
+                            currentSectionIndex = self.pictureForYearMonth.count - 1
+                        } else if let yearMonthIndex = self.pictureForYearMonth.firstIndex(where: { $0.month == currentPictureMonth && $0.year == currentPictureYear }) {
+                            currentYearMonth = self.pictureForYearMonth[yearMonthIndex]
+                            currentSectionIndex = yearMonthIndex
+                        } else {
+                            currentYearMonth = YearMonthPictures(referenceDate: picture.lastModifiedDate)
+                            self.pictureForYearMonth.append(currentYearMonth)
+                            currentSectionIndex = self.pictureForYearMonth.count - 1
+                            self.collectionView.insertSections([currentSectionIndex + 1])
+                        }
+                        currentYearMonth.pictures.append(picture)
+                        self.collectionView.insertItems(at: [IndexPath(row: currentYearMonth.pictures.count - 1, section: currentSectionIndex + 1)])
+                    }
+                } completion: { done in
+
+                }
+
+                self.pictures += fetchedPictures
                 self.showEmptyView(.noImages)
                 self.page += 1
-                self.hasNextPage = data.count == DriveApiFetcher.itemPerPage
-                self.driveFileManager.setLocalFiles(self.files, root: DriveFileManager.lastPicturesRootFile)
+                self.hasNextPage = fetchedPictures.count == DriveApiFetcher.itemPerPage
+                self.driveFileManager.setLocalFiles(self.pictures, root: DriveFileManager.lastPicturesRootFile)
             }
-            self.footerView?.isHidden = true
             self.isLoading = false
-            if self.files.isEmpty && ReachabilityListener.instance.currentStatus == .offline {
+            if self.pictureForYearMonth.isEmpty && ReachabilityListener.instance.currentStatus == .offline {
                 self.hasNextPage = false
                 self.showEmptyView(.noNetwork, showButton: true)
             }
@@ -135,7 +182,7 @@ class PhotoListViewController: UIViewController {
     }
 
     func showEmptyView(_ type: EmptyTableView.EmptyTableViewType, showButton: Bool = false) {
-        if files.isEmpty {
+        if pictureForYearMonth.isEmpty {
             let background = EmptyTableView.instantiate(type: type, button: showButton, setCenteringEnabled: true)
             background.actionHandler = { _ in
                 self.forceRefresh()
@@ -187,39 +234,71 @@ class PhotoListViewController: UIViewController {
 
 // MARK: - UICollectionViewDelegate, UICollectionViewDataSource
 extension PhotoListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return pictureForYearMonth.count + 1
+    }
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return files.count
+        if section == 0 {
+            return 0
+        } else {
+            return pictureForYearMonth[section - 1].pictures.count
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(type: HomeLastPicCollectionViewCell.self, for: indexPath)
-        cell.configureWith(file: files[indexPath.row])
+        cell.configureWith(file: pictureForYearMonth[indexPath.section - 1].pictures[indexPath.row])
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: 80)
+        if section == numberOfSections(in: collectionView) - 1 && isLoading {
+            return CGSize(width: collectionView.frame.width, height: 80)
+        } else {
+            return CGSize.zero
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 0 {
+            return CGSize.zero
+        } else {
+            return CGSize(width: collectionView.frame.width, height: 50)
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        if footerView == nil {
+        if kind == UICollectionView.elementKindSectionFooter {
             let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: footerIdentifier, for: indexPath)
             let indicator = UIActivityIndicatorView(style: .gray)
+            indicator.hidesWhenStopped = true
             indicator.color = KDriveAsset.loaderDarkerDefaultColor.color
-            indicator.startAnimating()
+            if isLoading {
+                indicator.startAnimating()
+            } else {
+                indicator.stopAnimating()
+            }
             footerView.addSubview(indicator)
             indicator.translatesAutoresizingMaskIntoConstraints = false
             NSLayoutConstraint.activate([
                 indicator.centerXAnchor.constraint(equalTo: footerView.centerXAnchor),
                 indicator.centerYAnchor.constraint(equalTo: footerView.centerYAnchor)
                 ])
-            self.footerView = footerView
+            return footerView
+        } else {
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as! PhotoSectionHeaderView
+            if indexPath.section > 0 {
+                let yearMonth = pictureForYearMonth[indexPath.section - 1]
+                headerView.titleLabel.text = dateFormatter.string(from: yearMonth.referenceDate)
+            }
+            return headerView
         }
-        return footerView!
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        filePresenter.present(driveFileManager: driveFileManager, file: files[indexPath.row], files: files, normalFolderHierarchy: false)
+        filePresenter.present(driveFileManager: driveFileManager, file: pictureForYearMonth[indexPath.section - 1].pictures[indexPath.row], files: pictures, normalFolderHierarchy: false)
     }
 }
 
@@ -229,7 +308,7 @@ extension PhotoListViewController: UICollectionViewDelegateFlowLayout {
         guard let collectionViewLayout = collectionViewLayout as? UICollectionViewFlowLayout else {
             return .zero
         }
-        let width = collectionView.frame.width - collectionViewLayout.minimumInteritemSpacing * CGFloat(numberOfColumns - 1)
+        let width = collectionView.frame.width - 16 - collectionViewLayout.minimumInteritemSpacing * CGFloat(numberOfColumns - 1)
         let cellWidth = width / CGFloat(numberOfColumns)
         return CGSize(width: cellWidth, height: cellWidth)
     }
