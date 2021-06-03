f7d50a6a033ead4daeb52b49a630ef791a83634d
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2021-06-03 16:54:22 +0200

    fix on DriveFileManager realm
    Signed-off-by: Ambroise Decouttere <ambroise.decouttere@infomaniak.com>

--- kDrive/UI/Controller/Menu/LastModificationsViewController.swift
+++ kDrive/UI/Controller/Menu/LastModificationsViewController.swift
@@ -42,11 +42,11 @@ class LastModificationsViewController: FileListViewController {
         }
 
         if currentDirectory.id == DriveFileManager.lastModificationsRootFile.id {
-            driveFileManager.apiFetcher.getLastModifiedFiles(page: page) { (response, error) in
-                if let data = response?.data {
-                    completion(.success(data), data.count == DriveApiFetcher.itemPerPage, false)
+            driveFileManager.getLastModifiedFiles(page: page) { (response, error) in
+                if let files = response {
+                    completion(.success(files), files.count == DriveApiFetcher.itemPerPage, true)
                 } else {
-                    completion(.failure(error ?? DriveError.localError), false, false)
+                    completion(.failure(error ?? DriveError.localError), false, true)
                 }
             }
         } else {
