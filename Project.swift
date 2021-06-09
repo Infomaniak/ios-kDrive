cbef8c469ebe4520f6af167c3c85534b39460386
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2021-06-09 09:07:02 +0200

    4.0.5 - Build 2
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- Project.swift
+++ Project.swift
@@ -21,7 +21,7 @@ import ProjectDescription
 let baseSettings: [String: SettingValue] = [
     "CODE_SIGN_STYLE": "Automatic",
     "DEVELOPMENT_TEAM": "864VDCS2QY",
-    "CURRENT_PROJECT_VERSION": "1",
+    "CURRENT_PROJECT_VERSION": "2",
     "MARKETING_VERSION": "4.0.5"
 ]
 
