5095561b3552d54fab7fc8aef7ca0c257a80ccf0
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2021-06-22 09:22:53 +0200

    fullscreen preview fixed for audio and video
    Signed-off-by: Ambroise Decouttere <ambroise.decouttere@infomaniak.com>

--- kDrive/UI/Controller/Files/Preview/PreviewViewController.swift
+++ kDrive/UI/Controller/Files/Preview/PreviewViewController.swift
@@ -492,12 +492,14 @@ extension PreviewViewController: UICollectionViewDataSource {
                 cell.parentViewController = floatingPanelViewController
                 cell.driveFileManager = driveFileManager
                 cell.configureWith(file: file)
+                cell.previewFrameImageView.addGestureRecognizer(tap)
                 return cell
             case .audio:
                 let cell = collectionView.dequeueReusableCell(type: AudioCollectionViewCell.self, for: indexPath)
                 cell.previewDelegate = self
                 cell.driveFileManager = driveFileManager
                 cell.configureWith(file: file)
+                cell.addGestureRecognizer(tap)
                 return cell
             case .spreadsheet, .presentation, .text:
                 let cell = collectionView.dequeueReusableCell(type: OfficePreviewCollectionViewCell.self, for: indexPath)
