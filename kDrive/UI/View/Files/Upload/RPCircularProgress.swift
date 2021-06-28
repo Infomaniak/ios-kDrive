6db4c7063db0390c7e38695522a92bcc182d18cf
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2021-06-28 16:42:04 +0200

    Fix file header
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- kDrive/UI/View/Files/Upload/RPCircularProgress.swift
+++ kDrive/UI/View/Files/Upload/RPCircularProgress.swift
@@ -1,12 +1,20 @@
-//
-//  RPCircularProgress.swift
-//  RPCircularProgress
-//
-//  Created by Rob Phillips on 4/5/16.
-//  Copyright © 2016 Glazed Donut, LLC. All rights reserved.
-//
-//  See LICENSE for full license agreement.
-//
+/*
+ Infomaniak kDrive - iOS App
+ Copyright (C) 2021 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import UIKit
 
