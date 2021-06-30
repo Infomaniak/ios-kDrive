3a40a31f3f7884260a6dea395298f6b1f2eb896e
Florentin Bekier <florentin.bekier@infomaniak.com>
2021-06-30 14:43:35 +0200

    Fix photo sort view
    Signed-off-by: Florentin Bekier <florentin.bekier@infomaniak.com>

--- kDrive/UI/Controller/Menu/FloatingPanelPhotoSortViewController.swift
+++ kDrive/UI/Controller/Menu/FloatingPanelPhotoSortViewController.swift
@@ -1,31 +1,30 @@
 /*
-Infomaniak kDrive - iOS App
-Copyright (C) 2021 Infomaniak Network SA
+ Infomaniak kDrive - iOS App
+ Copyright (C) 2021 Infomaniak Network SA
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-import UIKit
 import FloatingPanel
 import kDriveCore
+import UIKit
 
 protocol PhotoSortDelegate: AnyObject {
     func didSelect(sortMode: PhotoSortMode)
 }
 
 class FloatingPanelPhotoSortViewController: UITableViewController, FloatingPanelControllerDelegate {
-
     let tableContent: [PhotoSortMode] = PhotoSortMode.allCases
 
     var selectedSortMode: PhotoSortMode!
@@ -37,7 +36,11 @@ class FloatingPanelPhotoSortViewController: UITableViewController, FloatingPanel
         tableView.register(cellView: FloatingPanelSortOptionTableViewCell.self)
         tableView.separatorColor = .clear
         tableView.backgroundColor = KDriveAsset.backgroundCardViewColor.color
-        let selectedIndex = (tableContent.firstIndex(where: { $0 == selectedSortMode }) ?? 0) + 1
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let selectedIndex = (tableContent.firstIndex { $0 == selectedSortMode } ?? 0) + 1
         tableView.selectRow(at: IndexPath(row: selectedIndex, section: 0), animated: false, scrollPosition: .none)
     }
 
@@ -52,15 +55,19 @@ class FloatingPanelPhotoSortViewController: UITableViewController, FloatingPanel
             cell.isHeader = true
         } else {
             cell.titleLabel.text = tableContent[indexPath.row - 1].title
-            cell.separator?.isHidden = true
             cell.isHeader = false
         }
-        cell.setAccessibility()
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if indexPath.row == 0 {
+            return nil
+        }
+        return indexPath
+    }
+
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard indexPath.row != 0 else { return }
         delegate?.didSelect(sortMode: tableContent[indexPath.row - 1])
         dismiss(animated: true)
     }
