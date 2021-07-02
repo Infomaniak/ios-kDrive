7c08b0245225363a9c8160345593bac38f9ba894
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2021-07-02 10:46:48 +0200

    Register for topic
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- kDriveCore/Data/MQService/MQService.swift
+++ kDriveCore/Data/MQService/MQService.swift
@@ -21,65 +21,79 @@ import CocoaMQTTWebSocket
 import Foundation
 
 public class MQService {
+    private let webSocket: CocoaMQTTWebSocket
+    private let mqtt: CocoaMQTT
+    private var currentToken: IPSToken?
+
     public init() {
-        let websocket = CocoaMQTTWebSocket(uri: "/ws")
-        websocket.enableSSL = true
-        let mqtt = CocoaMQTT(clientID: "", host: "info-mq.infomaniak.com", port: 443, socket: websocket)
+        webSocket = CocoaMQTTWebSocket(uri: "/ws")
+        webSocket.enableSSL = true
+        mqtt = CocoaMQTT(clientID: "", host: "info-mq.infomaniak.com", port: 443, socket: webSocket)
         mqtt.username = "ips:ips-public"
         mqtt.password = "8QC5EwBqpZ2Z"
         mqtt.delegate = self
         mqtt.keepAlive = 30
-        mqtt.connect(timeout: 20)
+        _ = mqtt.connect(timeout: 20)
+    }
+
+    public func registerForNotifications(with token: IPSToken) {
+        if let currentToken = currentToken {
+            mqtt.unsubscribe(topicFor(token: currentToken))
+        }
+        currentToken = token
+        if !isSubscribed(token: token) {
+            mqtt.subscribe(topicFor(token: token))
+        }
+    }
+
+    private func topicFor(token: IPSToken) -> String {
+        return "drive/\(token.uuid)"
+    }
+
+    private func isSubscribed(token: IPSToken) -> Bool {
+        return mqtt.subscriptions[topicFor(token: token)] != nil
     }
 }
 
 extension MQService: CocoaMQTTDelegate {
     public func mqtt(_ mqtt: CocoaMQTT, didReceive trust: SecTrust, completionHandler: @escaping (Bool) -> Void) {
-        print("trust: \(trust)")
         completionHandler(true)
     }
 
     public func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
-        print("ack: \(ack)")
-
         if ack == .accept {
-            mqtt.subscribe("drive/5e1f23fe-614b-416d-b0ac-a4fb7edebe8c", qos: CocoaMQTTQoS.qos1)
+            if let currentToken = currentToken {
+                registerForNotifications(with: currentToken)
+            }
         }
     }
 
     public func mqtt(_ mqtt: CocoaMQTT, didStateChangeTo state: CocoaMQTTConnState) {
-        print("new state: \(state)")
+        // TODO: Handle disconnect
     }
 
-    public func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
-        print("message: \(message.string?.description), id: \(id)")
-    }
+    public func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {}
 
-    public func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
-        print("id: \(id)")
-    }
+    public func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {}
 
     public func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16) {
-        print("message: \(message.string?.description), id: \(id)")
+        let data = Data(message.payload)
+        if let message = String(data: data, encoding: .utf8) {
+            print(message)
+        }
     }
 
     public func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopics success: NSDictionary, failed: [String]) {
         print("subscribed: \(success), failed: \(failed)")
     }
 
-    public func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopics topics: [String]) {
-        print("topic: \(topics)")
-    }
+    public func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopics topics: [String]) {}
 
-    public func mqttDidPing(_ mqtt: CocoaMQTT) {
-        print()
-    }
+    public func mqttDidPing(_ mqtt: CocoaMQTT) {}
 
-    public func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
-        print()
-    }
+    public func mqttDidReceivePong(_ mqtt: CocoaMQTT) {}
 
     public func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
-        print("\(err?.localizedDescription)")
+        // TODO: Handle disconnect
     }
 }
