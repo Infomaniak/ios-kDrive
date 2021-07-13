5180f4630a517c0dc7285f6b43d450da563c4315
Florentin Bekier <florentin.bekier@infomaniak.com>
2021-07-13 15:19:35 +0200

    Remove photo sync disabled log
    Signed-off-by: Florentin Bekier <florentin.bekier@infomaniak.com>

--- kDriveCore/Data/UploadQueue/PhotoLibraryUploader.swift
+++ kDriveCore/Data/UploadQueue/PhotoLibraryUploader.swift
@@ -20,7 +20,6 @@ import CocoaLumberjackSwift
 import Foundation
 import Photos
 import RealmSwift
-import Sentry
 
 public class PhotoLibraryUploader {
     public static let instance = PhotoLibraryUploader()
@@ -60,7 +59,6 @@ public class PhotoLibraryUploader {
     }
 
     public func disableSync(using realm: Realm = DriveFileManager.constants.uploadsRealm) {
-        SentrySDK.capture(message: "Photo sync disabled")
         try? realm.write {
             realm.delete(realm.objects(PhotoSyncSettings.self))
         }
