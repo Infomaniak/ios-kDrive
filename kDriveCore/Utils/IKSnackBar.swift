acd66e92332d693f73161dd59eebe4c3bbbca68d
Florentin Bekier <florentin.bekier@infomaniak.com>
2021-07-20 16:04:26 +0200

    WIP UIWindow for snackbar
    Signed-off-by: Florentin Bekier <florentin.bekier@infomaniak.com>

--- kDriveCore/Utils/IKSnackBar.swift
+++ kDriveCore/Utils/IKSnackBar.swift
@@ -16,8 +16,62 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import UIKit
 import SnackBar
+import UIKit
+
+class IKWindow: UIWindow {
+    init(with rootVC: UIViewController) {
+        if #available(iOS 13.0, *) {
+            if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                super.init(windowScene: scene)
+            } else {
+                super.init(frame: UIScreen.main.bounds)
+            }
+        } else {
+            super.init(frame: UIScreen.main.bounds)
+        }
+        backgroundColor = .clear
+        rootViewController = rootVC
+        accessibilityViewIsModal = true
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if let rootViewController = IKWindowProvider.shared.rootViewController,
+           let view = rootViewController.view.subviews.first {
+            return view.hitTest(point, with: event)
+        }
+
+        return nil
+    }
+}
+
+public class IKWindowProvider {
+    public static let shared = IKWindowProvider()
+
+    var entryWindow: IKWindow!
+    var rootViewController: UIViewController? {
+        return entryWindow?.rootViewController
+    }
+
+    private init() {}
+
+    func setupWindowAndRootVC() -> UIViewController {
+        let entryViewController: UIViewController
+        if entryWindow == nil {
+            entryViewController = UIViewController()
+            entryWindow = IKWindow(with: entryViewController)
+            entryWindow.isHidden = false
+        } else {
+            entryViewController = rootViewController!
+        }
+        return entryViewController
+    }
+}
 
 public class IKSnackBar: SnackBar {
     public struct Action {
@@ -32,14 +86,15 @@ public class IKSnackBar: SnackBar {
 
     required init(contextView: UIView, message: String, duration: Duration) {
         super.init(contextView: contextView, message: message, duration: duration)
-        self.addShadow(elevation: 6)
+        addShadow(elevation: 6)
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    public override var style: SnackBarStyle {
+    override public var style: SnackBarStyle {
         let textStyle = TextStyle.subtitle2
         let buttonStyle = TextStyle.action
         var style = SnackBarStyle()
@@ -65,10 +120,10 @@ public class IKSnackBar: SnackBar {
 
     private static func getVisibleViewController(from viewController: UIViewController) -> UIViewController {
         if let navigationController = viewController as? UINavigationController,
-            let visibleController = navigationController.visibleViewController {
+           let visibleController = navigationController.visibleViewController {
             return getVisibleViewController(from: visibleController)
         } else if let tabBarController = viewController as? UITabBarController,
-            let selectedTabController = tabBarController.selectedViewController {
+                  let selectedTabController = tabBarController.selectedViewController {
             return getVisibleViewController(from: selectedTabController)
         } else {
             if let presentedViewController = viewController.presentedViewController {
@@ -83,12 +138,17 @@ public class IKSnackBar: SnackBar {
         if let view = view {
             return Self.make(in: view, message: message, duration: duration)
         } else {
-            guard let vc = getTopViewController() else { return nil }
+            let vc = IKWindowProvider.shared.setupWindowAndRootVC()
             return Self.make(in: vc.view, message: message, duration: duration)
         }
     }
 
     public func setAction(_ action: Action) -> SnackBarPresentable {
         return setAction(with: action.title, action: action.action)
+    }
+
+    public override func removeFromSuperview() {
+        super.removeFromSuperview()
+        // Remove window
     }
 }
