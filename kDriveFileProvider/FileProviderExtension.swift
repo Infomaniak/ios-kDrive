229a55d370d7fca24da0119da64f9fdab79d96df
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2021-08-06 15:04:10 +0200

    Format FileProviderExtension
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- kDriveFileProvider/FileProviderExtension.swift
+++ kDriveFileProvider/FileProviderExtension.swift
@@ -18,11 +18,10 @@
 
 import CoreServices
 import FileProvider
-import kDriveCore
 import InfomaniakLogin
+import kDriveCore
 
 class FileProviderExtensionState {
-
     static let shared = FileProviderExtensionState()
     var importedDocuments = [NSFileProviderItemIdentifier: FileProviderItem]()
     var workingSet = [NSFileProviderItemIdentifier: FileProviderItem]()
@@ -44,7 +43,6 @@ class FileProviderExtensionState {
 }
 
 class FileProviderExtension: NSFileProviderExtension {
-
     lazy var fileCoordinator: NSFileCoordinator = {
         let fileCoordinator = NSFileCoordinator()
         fileCoordinator.purposeIdentifier = manager.providerIdentifier
@@ -62,8 +60,8 @@ class FileProviderExtension: NSFileProviderExtension {
 
     private func setDriveFileManager() -> DriveFileManager? {
         if let objectId = domain?.identifier.rawValue,
-            let drive = DriveInfosManager.instance.getDrive(objectId: objectId),
-            let driveFileManager = accountManager.getDriveFileManager(for: drive) {
+           let drive = DriveInfosManager.instance.getDrive(objectId: objectId),
+           let driveFileManager = accountManager.getDriveFileManager(for: drive) {
             return driveFileManager
         } else {
             return accountManager.currentDriveFileManager
@@ -92,7 +90,7 @@ class FileProviderExtension: NSFileProviderExtension {
         } else if let item = FileProviderExtensionState.shared.importedDocuments[identifier] {
             return item
         } else if let fileId = identifier.toFileId(),
-            let file = driveFileManager.getCachedFile(id: fileId) {
+                  let file = driveFileManager.getCachedFile(id: fileId) {
             return FileProviderItem(file: file, domain: domain)
         } else {
             throw nsError(code: .noSuchItem)
@@ -103,7 +101,7 @@ class FileProviderExtension: NSFileProviderExtension {
         if let item = FileProviderExtensionState.shared.importedDocuments[identifier] {
             return item.storageUrl
         } else if let fileId = identifier.toFileId(),
-            let file = driveFileManager.getCachedFile(id: fileId) {
+                  let file = driveFileManager.getCachedFile(id: fileId) {
             return FileProviderItem(file: file, domain: domain).storageUrl
         } else {
             return nil
@@ -116,7 +114,7 @@ class FileProviderExtension: NSFileProviderExtension {
 
     override func providePlaceholder(at url: URL, completionHandler: @escaping (_ error: Error?) -> Void) {
         guard let fileId = FileProviderItem.identifier(for: url, domain: domain)?.toFileId(),
-            let file = driveFileManager.getCachedFile(id: fileId) else {
+              let file = driveFileManager.getCachedFile(id: fileId) else {
             completionHandler(nsError(code: .noSuchItem))
             return
         }
@@ -125,7 +123,7 @@ class FileProviderExtension: NSFileProviderExtension {
             try? FileManager.default.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true, attributes: nil)
             let placeholderURL = NSFileProviderManager.placeholderURL(for: url)
             try NSFileProviderManager.writePlaceholder(at: placeholderURL, withMetadata: FileProviderItem(file: file, domain: domain))
-        } catch let error {
+        } catch {
             print("\(error)")
         }
         completionHandler(nil)
@@ -133,14 +131,14 @@ class FileProviderExtension: NSFileProviderExtension {
 
     override func itemChanged(at url: URL) {
         if let identifier = persistentIdentifierForItem(at: url),
-            let item = try? item(for: identifier) as? FileProviderItem {
+           let item = try? item(for: identifier) as? FileProviderItem {
             backgroundUploadItem(item)
         }
     }
 
     override func startProvidingItem(at url: URL, completionHandler: @escaping (Error?) -> Void) {
         guard let fileId = FileProviderItem.identifier(for: url, domain: domain)?.toFileId(),
-            let file = driveFileManager.getCachedFile(id: fileId) else {
+              let file = driveFileManager.getCachedFile(id: fileId) else {
             if FileManager.default.fileExists(atPath: url.path) {
                 completionHandler(nil)
             } else {
@@ -224,10 +222,10 @@ class FileProviderExtension: NSFileProviderExtension {
 
     override func stopProvidingItem(at url: URL) {
         if let identifier = persistentIdentifierForItem(at: url),
-            let item = try? item(for: identifier) as? FileProviderItem {
+           let item = try? item(for: identifier) as? FileProviderItem {
             if let remoteModificationDate = item.contentModificationDate,
-                let localModificationDate = try? item.storageUrl.resourceValues(forKeys: [.contentModificationDateKey]).contentModificationDate,
-                remoteModificationDate > localModificationDate {
+               let localModificationDate = try? item.storageUrl.resourceValues(forKeys: [.contentModificationDateKey]).contentModificationDate,
+               remoteModificationDate > localModificationDate {
                 backgroundUploadItem(item) {
                     self.cleanupAt(url: url)
                 }
@@ -238,7 +236,6 @@ class FileProviderExtension: NSFileProviderExtension {
             // The document isn't in realm maybe it was recently imported?
             cleanupAt(url: url)
         }
-
     }
 
     private func cleanupAt(url: URL) {
@@ -249,7 +246,7 @@ class FileProviderExtension: NSFileProviderExtension {
         }
 
         // write out a placeholder to facilitate future property lookups
-        self.providePlaceholder(at: url) { _ in
+        providePlaceholder(at: url) { _ in
             // TODO: handle any error, do any necessary cleanup
         }
     }
