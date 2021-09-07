ddc237a0495dcd44729fe3819b4757d00e174510
Florentin Bekier <florentin.bekier@infomaniak.com>
2021-09-07 09:08:16 +0200

    4.0.9 - Build 7
    Signed-off-by: Florentin Bekier <florentin.bekier@infomaniak.com>

--- Project.swift
+++ Project.swift
@@ -21,7 +21,7 @@ import ProjectDescription
 let baseSettings: [String: SettingValue] = [
     "CODE_SIGN_STYLE": "Automatic",
     "DEVELOPMENT_TEAM": "864VDCS2QY",
-    "CURRENT_PROJECT_VERSION": "5",
+    "CURRENT_PROJECT_VERSION": "7",
     "MARKETING_VERSION": "4.0.9"
 ]
 
