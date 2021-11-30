f49622f5257c2c7e9f2ac3210f9d62f911b8a238
Florentin Bekier <florentin.bekier@infomaniak.com>
2021-11-30 10:16:50 +0100

    Migrate Tuist to v2 + fix paths
    Signed-off-by: Florentin Bekier <florentin.bekier@infomaniak.com>

--- scripts/fix-tuist.sh
+++ scripts/fix-tuist.sh
@@ -1,7 +1,7 @@
-echo "" > Derived/Sources/Bundle+kDriveActionExtension.swift
-echo "" > Derived/Sources/Assets+KDriveActionExtension.swift
-echo "" > Derived/Sources/Strings+KDriveActionExtension.swift
+echo "" > $SRCROOT/Derived/Sources/Bundle+kDriveActionExtension.swift
+echo "" > $SRCROOT/Derived/Sources/Assets+KDriveActionExtension.swift
+echo "" > $SRCROOT/Derived/Sources/Strings+KDriveActionExtension.swift
 
-echo "" > Derived/Sources/Bundle+kDriveShareExtension.swift
-echo "" > Derived/Sources/Assets+kDriveShareExtension.swift
-echo "" > Derived/Sources/Strings+kDriveShareExtension.swift
+echo "" > $SRCROOT/Derived/Sources/Bundle+kDriveShareExtension.swift
+echo "" > $SRCROOT/Derived/Sources/Assets+kDriveShareExtension.swift
+echo "" > $SRCROOT/Derived/Sources/Strings+kDriveShareExtension.swift
