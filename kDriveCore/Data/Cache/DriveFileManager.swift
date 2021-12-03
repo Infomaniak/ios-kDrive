db87aa609c4321e88d3ef4ab5bcfd5dbe943acf0
Elena Willen <elena.willen@infomaniak.com>
2021-12-03 14:12:51 +0100

    update realm version
    Signed-off-by: Elena Willen <elena.willen@infomaniak.com>

--- kDriveCore/Data/Cache/DriveFileManager.swift
+++ kDriveCore/Data/Cache/DriveFileManager.swift
@@ -147,7 +147,7 @@ public class DriveFileManager {
         let realmName = "\(drive.userId)-\(drive.id).realm"
         realmConfiguration = Realm.Configuration(
             fileURL: DriveFileManager.constants.rootDocumentsURL.appendingPathComponent(realmName),
-            schemaVersion: 5,
+            schemaVersion: 6,
             migrationBlock: { migration, oldSchemaVersion in
                 if oldSchemaVersion < 1 {
                     // Migration to version 1: migrating rights
