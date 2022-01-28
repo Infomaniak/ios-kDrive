20285f4551d1adf47ed5655e6df2871452361cdd
Florentin Bekier <florentin.bekier@infomaniak.com>
2022-01-28 15:05:19 +0100

    Add file version
    Signed-off-by: Florentin Bekier <florentin.bekier@infomaniak.com>

--- kDrive/UI/Controller/Files/FileDetailViewController.swift
+++ kDrive/UI/Controller/Files/FileDetailViewController.swift
@@ -78,9 +78,9 @@ class FileDetailViewController: UIViewController {
             if file.size != 0 {
                 rows.append(.size)
             }
-            /* if file.sizeWithVersion != 0 {
+            if file.version != nil {
                 rows.append(.sizeAll)
-            } */
+            }
             return rows
         }
     }
@@ -195,7 +195,11 @@ class FileDetailViewController: UIViewController {
         let group = DispatchGroup()
         group.enter()
         Task {
+            do {
             self.file = try await driveFileManager.file(id: file.id, forceRefresh: true)
+            } catch {
+                debugPrint(error)
+            }
             group.leave()
         }
         group.enter()
