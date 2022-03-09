7b264c0bf2861274900d3885430ef3678940f72c
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2022-03-09 15:06:28 +0100

    Use InfomaniakCore for IKLabel & IKButton
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- kDrive/UI/View/Menu/PhotoList/PhotoSectionHeaderView.swift
+++ kDrive/UI/View/Menu/PhotoList/PhotoSectionHeaderView.swift
@@ -16,11 +16,11 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import UIKit
+import InfomaniakCore
 import kDriveCore
+import UIKit
 
 class PhotoSectionHeaderView: UICollectionReusableView {
-
     @IBOutlet weak var titleLabel: IKLabel!
 
     override func prepareForReuse() {
