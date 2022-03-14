a1e6b14a774f15e41ac4f8029201562668698f47
Valentin Perignon <valentin.perignon@infomaniak.com>
2022-03-14 10:25:43 +0100

    Collection View functions
    Signed-off-by: Valentin Perignon <valentin.perignon@infomaniak.com>

--- kDrive/UI/Controller/Files/MultipleSelectionFloatingPanelViewController.swift
+++ kDrive/UI/Controller/Files/MultipleSelectionFloatingPanelViewController.swift
@@ -60,6 +60,7 @@ class MultipleSelectionFloatingPanelViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.register(cellView: FloatingPanelActionCollectionViewCell.self)
         collectionView.alwaysBounceVertical = false
         setupContent()
     }
@@ -383,6 +384,14 @@ class MultipleSelectionFloatingPanelViewController: UICollectionViewController {
     }
 
     // MARK: - Collection view data source
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return actions.count
+    }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(type: FloatingPanelActionCollectionViewCell.self, for: indexPath)
