4ddbe55a1195942eb2b974f7944c907cafdc4d01
Valentin Perignon <valentin.perignon@infomaniak.com>
2022-06-21 16:21:38 +0200

    Add move action to selectAll and bulkAction
    Signed-off-by: Valentin Perignon <valentin.perignon@infomaniak.com>

--- kDrive/UI/Controller/Files/FileActionsFloatingPanelViewController.swift
+++ kDrive/UI/Controller/Files/FileActionsFloatingPanelViewController.swift
@@ -97,11 +97,11 @@ public class FloatingPanelAction: Equatable {
     }
 
     static var multipleSelectionBulkActions: [FloatingPanelAction] {
-        return [offline, download, duplicate].map { $0.reset() }
+        return [offline, download, move, duplicate].map { $0.reset() }
     }
 
     static var selectAllActions: [FloatingPanelAction] {
-        return [download, duplicate].map { $0.reset() }
+        return [download, move, duplicate].map { $0.reset() }
     }
 
     public static func == (lhs: FloatingPanelAction, rhs: FloatingPanelAction) -> Bool {
