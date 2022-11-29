c868048d070474c180193124e33105a472c3b4c6
Valentin Perignon <valentin.perignon@infomaniak.com>
2022-11-29 09:20:18 +0100

    Check dropbox right
    Signed-off-by: Valentin Perignon <valentin.perignon@infomaniak.com>

--- kDrive/UI/Controller/Files/FileActionsFloatingPanelViewController.swift
+++ kDrive/UI/Controller/Files/FileActionsFloatingPanelViewController.swift
@@ -396,7 +396,7 @@ class FileActionsFloatingPanelViewController: UICollectionViewController {
                 }
             }
         case .convertToDropbox:
-            if driveFileManager.drive.isFreePack {
+            if !file.capabilities.canBecomeDropbox {
                 let driveFloatingPanelController = DropBoxFloatingPanelViewController.instantiatePanel()
                 let floatingPanelViewController = driveFloatingPanelController.contentViewController as? DropBoxFloatingPanelViewController
                 floatingPanelViewController?.rightButton.isEnabled = driveFileManager.drive.accountAdmin
