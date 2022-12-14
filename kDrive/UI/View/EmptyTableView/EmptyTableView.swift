937a809e87b7d3e6323b062670d303e965a4336f
Valentin Perignon <valentin.perignon@infomaniak.com>
2022-12-14 13:50:36 +0100

    Feat: update translations
    Signed-off-by: Valentin Perignon <valentin.perignon@infomaniak.com>

--- kDrive/UI/View/EmptyTableView/EmptyTableView.swift
+++ kDrive/UI/View/EmptyTableView/EmptyTableView.swift
@@ -81,9 +81,9 @@ class EmptyTableView: UIView {
         case .noTrash:
             view = self.instantiate(logo: KDriveResourcesAsset.delete.image, message: KDriveResourcesStrings.Localizable.trashNoFile)
         case .emptyFolder:
-            view = self.instantiate(logo: KDriveResourcesAsset.folderFilled.image, message: KDriveResourcesStrings.Localizable.noFilesDescription)
+            view = self.instantiate(logo: KDriveResourcesAsset.folderFilled.image, message: KDriveResourcesStrings.Localizable.noFilesDescriptionWithCreationRights)
         case .emptyFolderSelectFolder:
-            view = self.instantiate(logo: KDriveResourcesAsset.folderFilled.image, message: KDriveResourcesStrings.Localizable.noFilesDescriptionSelectFolder)
+            view = self.instantiate(logo: KDriveResourcesAsset.folderFilled.image, message: KDriveResourcesStrings.Localizable.noFilesDescription)
         case .noFavorite:
             view = self.instantiate(logo: KDriveResourcesAsset.favorite.image, message: KDriveResourcesStrings.Localizable.favoritesNoFile)
             view.emptyImageView.tintColor = KDriveResourcesAsset.favoriteColor.color
