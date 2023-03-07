581b5b5436a4c7af51bfe74c83de45132664ed2e
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2023-03-07 14:08:02 +0100

    fix: better fix for glitching view on update notification in UploadQueueFoldersViewController

--- kDrive/UI/Controller/Files/Upload/UploadQueueFoldersViewController.swift
+++ kDrive/UI/Controller/Files/Upload/UploadQueueFoldersViewController.swift
@@ -81,13 +81,21 @@ class UploadQueueFoldersViewController: UITableViewController {
                         return
                     }
 
+                    // No animation on updating the same lines
+                    let rowAnimation: UITableView.RowAnimation
+                    if deletions == insertions, modifications.isEmpty {
+                        rowAnimation = .none
+                    } else {
+                        rowAnimation = .automatic
+                    }
+
                     self.tableView.performBatchUpdates {
                         self.updateFolders(from: results)
                         // Always apply updates in the following order: deletions, insertions, then modifications.
                         // Handling insertions before deletions may result in unexpected behavior.
-                        self.tableView.deleteRows(at: deletions.map { IndexPath(row: $0, section: 0) }, with: .automatic)
-                        self.tableView.insertRows(at: insertions.map { IndexPath(row: $0, section: 0) }, with: .automatic)
-                        self.tableView.reloadRows(at: modifications.map { IndexPath(row: $0, section: 0) }, with: .automatic)
+                        self.tableView.deleteRows(at: deletions.map { IndexPath(row: $0, section: 0) }, with: rowAnimation)
+                        self.tableView.insertRows(at: insertions.map { IndexPath(row: $0, section: 0) }, with: rowAnimation)
+                        self.tableView.reloadRows(at: modifications.map { IndexPath(row: $0, section: 0) }, with: rowAnimation)
                     }
                 case .error(let error):
                     DDLogError("Realm observer error: \(error)")
