325f6341dce09a509b212e017b7c06c1d59bf341
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2023-03-28 15:19:44 +0200

    chore: 4.3.0 - Build 5
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- Tuist/ProjectDescriptionHelpers/Constants.swift
+++ Tuist/ProjectDescriptionHelpers/Constants.swift
@@ -21,7 +21,7 @@ import ProjectDescription
 public enum Constants {
     public static let baseSettings = SettingsDictionary()
         .automaticCodeSigning(devTeam: "864VDCS2QY")
-        .currentProjectVersion("4")
+        .currentProjectVersion("5")
         .marketingVersion("4.3.0")
 
     public static let deploymentTarget = DeploymentTarget.iOS(targetVersion: "13.4", devices: [.iphone, .ipad])
