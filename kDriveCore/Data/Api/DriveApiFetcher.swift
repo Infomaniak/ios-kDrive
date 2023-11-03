3c2720969e45d6370491c9a9ebae3ba440718750
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2023-11-03 13:12:01 +0100

    feat: Cursored search + gallery
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- kDriveCore/Data/Api/DriveApiFetcher.swift
+++ kDriveCore/Data/Api/DriveApiFetcher.swift
@@ -402,9 +402,9 @@ public class DriveApiFetcher: ApiFetcher {
         fileTypes: [ConvertedType] = [],
         categories: [Category],
         belongToAllCategories: Bool,
-        page: Int = 1,
+        cursor: String? = nil,
         sortType: SortType = .nameAZ
-    ) async throws -> [File] {
+    ) async throws -> (data: [File], response: ApiResponse<[File]>) {
         try await perform(request: authenticatedRequest(.search(
             drive: drive,
             query: query,
@@ -412,7 +412,9 @@ public class DriveApiFetcher: ApiFetcher {
             fileTypes: fileTypes,
             categories: categories,
             belongToAllCategories: belongToAllCategories
-        ).paginated(page: page).sorted(by: [sortType]))).data
+        )
+        .cursored(cursor)
+        .sorted(by: [sortType])))
     }
 
     public func add(category: Category, to file: ProxyFile) async throws -> CategoryResponse {
