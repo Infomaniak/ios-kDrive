7a166ed708d9c4affa8459bb6399cbe65fc05083
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2024-04-24 11:11:51 +0200

    refactor: Use factorised code

--- kDriveCore/Data/UploadQueue/Operation/UploadOperation.swift
+++ kDriveCore/Data/UploadQueue/Operation/UploadOperation.swift
@@ -269,29 +269,11 @@ public final class UploadOperation: AsynchronousOperation, UploadOperationable {
 
             // Remove source file if uploaded with success and required.
             if file.uploadDate != nil {
-                let removed = file.cleanSourceFileIfNeeded()
-                if removed {
+                if file.cleanSourceFileIfNeeded() {
                     Log.uploadOperation(
                         "Removed local file at path:\(String(describing: file.pathURL)) ufid:\(self.uploadFileId)"
                     )
-                } else {
-                    Log.uploadOperation(
-                        "Unable to remove local file at path:\(String(describing: file.pathURL)) ufid:\(self.uploadFileId)",
-                        level: .error
-                    )
                 }
-            } else {
-                Log.uploadOperation(
-                    "Not removing local file, shouldRemove:\(file.shouldRemoveAfterUpload)  at path:\(String(describing: file.pathURL)) ufid:\(self.uploadFileId)",
-                    level: .warning
-                )
-            }
-
-            if let path = file.pathURL,
-               file.shouldRemoveAfterUpload && file.uploadDate != nil {
-                Log.uploadOperation("Remove local file at path:\(path) ufid:\(self.uploadFileId)")
-                try? self.fileManager.removeItem(at: path)
-
             } else {
                 Log.uploadOperation(
                     "Not removing local file, shouldRemove:\(file.shouldRemoveAfterUpload)  at path:\(String(describing: file.pathURL)) ufid:\(self.uploadFileId)",
