305ccabb65d08973737528dd1e9a233e9b7cdff2
Valentin Perignon <valentin.perignon@infomaniak.com>
2024-06-07 11:50:53 +0200

    fix: Update IAP FAQ link
    Signed-off-by: Valentin Perignon <valentin.perignon@infomaniak.com>

--- kDriveCore/Utils/Constants.swift
+++ kDriveCore/Utils/Constants.swift
@@ -41,7 +41,7 @@ public struct URLConstants {
     public static let sourceCode = URLConstants(urlString: "https://github.com/Infomaniak/ios-kDrive")
     public static let gpl = URLConstants(urlString: "https://www.gnu.org/licenses/gpl-3.0.html")
     public static let support = URLConstants(urlString: "https://support.infomaniak.com")
-    public static let faqIAP = URLConstants(urlString: "https://faq.infomaniak.com/2631")
+    public static let faqIAP = URLConstants(urlString: "https://faq.infomaniak.com/2632")
     public static let matomo = URLConstants(urlString: "https://analytics.infomaniak.com/matomo.php")
 
     public static func renewDrive(accountId: Int) -> URLConstants {
