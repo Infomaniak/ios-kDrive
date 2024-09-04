1b82292e81b8b6c133428ad55f9158e28e45733e
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2024-09-04 14:11:23 +0200

    fix(DriveFileManager): Deleting a file was broken since 5.0.0-8 by #1260

--- kDriveCore/Data/Cache/DriveFileManager/DriveFileManager.swift
+++ kDriveCore/Data/Cache/DriveFileManager/DriveFileManager.swift
@@ -1018,8 +1018,15 @@ public final class DriveFileManager {
     }
 
     func removeFileInDatabase(fileUid: String, cascade: Bool, writableRealm: Realm) {
-        var fileUidsToProcess = [fileUid]
-        var liveFilesToDelete = [File]()
+        guard let rootLiveFile = writableRealm.object(ofType: File.self, forPrimaryKey: fileUid),
+              !rootLiveFile.isInvalidated else {
+            return
+        }
+
+        try? rootLiveFile.clearOnFileSystemIfNeeded()
+
+        var fileUidsToProcess: [String] = rootLiveFile.children.map(\.uid)
+        var liveFilesToDelete: [File] = [rootLiveFile]
 
         while !fileUidsToProcess.isEmpty {
             let currentFileUid = fileUidsToProcess.removeLast()
@@ -1027,9 +1034,7 @@ public final class DriveFileManager {
                 continue
             }
 
-            if fileManager.fileExists(atPath: file.localContainerUrl.path) {
-                try? fileManager.removeItem(at: file.localContainerUrl)
-            }
+            try? file.clearOnFileSystemIfNeeded()
 
             if cascade {
                 let filesUidsToDelete = liveFilesToDelete.map { $0.uid }
