d0257593103b9e79a48c9a916c536a1f50ce6abb
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2024-09-26 16:32:28 +0200

    feat: Download all files from current folder as a ZIP within a public share

--- kDrive/UI/View/Files/FileListBarButton.swift
+++ kDrive/UI/View/Files/FileListBarButton.swift
@@ -17,8 +17,8 @@
  */
 
 import Foundation
-import UIKit
 import kDriveResources
+import UIKit
 
 final class FileListBarButton: UIBarButtonItem {
     private(set) var type: FileListBarButtonType = .cancel
@@ -49,6 +49,10 @@ final class FileListBarButton: UIBarButtonItem {
         case .addFolder:
             self.init(image: KDriveResourcesAsset.folderAdd.image, style: .plain, target: target, action: action)
             accessibilityLabel = KDriveResourcesStrings.Localizable.createFolderTitle
+        case .downloadAll:
+            let image = KDriveResourcesAsset.download.image
+            self.init(image: image, style: .plain, target: target, action: action)
+            accessibilityLabel = KDriveResourcesStrings.Localizable.buttonDownload
         }
         self.type = type
     }
