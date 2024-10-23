7d8748fa40557e80558f01422944500c0b96691a
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2024-10-23 09:49:38 +0200

    feat: UnavaillableFolderViewController

--- kDrive/UI/Controller/Files/Rights and Share/UnavaillableFolderViewController.swift
+++ kDrive/UI/Controller/Files/Rights and Share/UnavaillableFolderViewController.swift
@@ -16,11 +16,9 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class LockedFolderViewController: UIViewController {
+class UnavaillableFolderViewController: UIViewController {
     viewDidLoad() {
         super.viewDidLoad()
-        title = "Locked Folder"
+        title = "Content Unavailable"
     }
 }
-
-
