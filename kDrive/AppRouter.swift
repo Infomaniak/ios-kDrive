1dea01e683f2e5c6f72e3ead2c49874cf2fa2b20
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2024-12-12 10:58:59 +0100

    fix: Allow disabled public share files

--- kDrive/AppRouter.swift
+++ kDrive/AppRouter.swift
@@ -652,16 +652,6 @@ public struct AppRouter: AppNavigable {
         }
 
         rootViewController.dismiss(animated: false) {
-            guard !frozenRootFolder.isDisabled else {
-                let noDriveViewController = NoDriveUpsaleViewController()
-                noDriveViewController.dismissCallback = {
-                    rootViewController.dismiss(animated: false) {}
-                }
-                let floatingPanel = UpsaleFloatingPanelController(upsaleViewController: noDriveViewController)
-                rootViewController.present(floatingPanel, animated: true, completion: nil)
-                return
-            }
-
             let viewModel = PublicShareViewModel(publicShareProxy: publicShareProxy,
                                                  sortType: .nameAZ,
                                                  driveFileManager: driveFileManager,
