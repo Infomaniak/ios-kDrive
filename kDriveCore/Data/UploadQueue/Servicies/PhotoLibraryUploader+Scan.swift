60e4848f6f88981129c93a6f258fa7d0bea5dd7b
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-01-16 10:38:15 +0100

    fix(PhotoLibraryUploader): Skip iCloud asset in error and continue to scan libray

--- kDriveCore/Data/UploadQueue/Servicies/PhotoLibraryUploader+Scan.swift
+++ kDriveCore/Data/UploadQueue/Servicies/PhotoLibraryUploader+Scan.swift
@@ -135,10 +135,8 @@ public extension PhotoLibraryUploader {
                 do {
                     bestResourceSHA256 = try asset.bestResourceSHA256
                 } catch {
-                    // Error thrown while hashing a resource, we stop ASAP.
+                    // Error thrown while hashing a resource, we skip the asset.
                     Log.photoLibraryUploader("Error while hashing:\(error) asset: \(asset.localIdentifier)", level: .error)
-                    writableRealm.cancelWrite()
-                    stop.pointee = true
                     return
                 }
 
