52005857162ad8acba9417ad0b4527be87ccba34
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-02-07 14:19:46 +0100

    feat(ios-features): Added necessary libraries as a Framework

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -20,7 +20,10 @@ let packageSettings = PackageSettings(
         "Kingfisher": .framework,
         "Lottie": .framework,
         "RealmSwift": .framework,
-        "Realm": .framework
+        "Realm": .framework,
+        "MyKSuite": .framework,
+        "InfomaniakCoreSwiftUI": .framework,
+        "DesignSystem": .framework
     ]
 )
 
