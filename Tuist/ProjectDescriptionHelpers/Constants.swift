4c1b354fa67e5938f93a13167045b245893ffaea
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-02-21 13:36:14 +0100

    chore: Bump app version to 5.2.1

--- Tuist/ProjectDescriptionHelpers/Constants.swift
+++ Tuist/ProjectDescriptionHelpers/Constants.swift
@@ -26,7 +26,7 @@ public enum Constants {
     public static let baseSettings = SettingsDictionary()
         .automaticCodeSigning(devTeam: "864VDCS2QY")
         .currentProjectVersion("1")
-        .marketingVersion("5.2.0")
+        .marketingVersion("5.2.1")
 
     public static let deploymentTarget = DeploymentTargets.iOS("15.0")
     public static let destinations = Set<Destination>([.iPhone, .iPad])
