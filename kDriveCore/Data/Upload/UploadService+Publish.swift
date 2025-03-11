173608823199274097982e83c1926c9597207d7c
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-03-11 08:53:56 +0100

    feat(UploadService): Publish

--- kDriveCore/Data/Upload/UploadService+Publish.swift
+++ kDriveCore/Data/Upload/UploadService+Publish.swift
@@ -1,6 +1,6 @@
 /*
  Infomaniak kDrive - iOS App
- Copyright (C) 2023 Infomaniak Network SA
+ Copyright (C) 2025 Infomaniak Network SA
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -21,9 +21,24 @@ import Foundation
 import InfomaniakCore
 import RealmSwift
 
+protocol UploadPublishable {
+    func publishUploadCount(withParent parentId: Int,
+                            userId: Int,
+                            driveId: Int)
+
+    func publishUploadCountInParent(parentId: Int,
+                                    userId: Int,
+                                    driveId: Int)
+
+    func publishUploadCountInDrive(userId: Int,
+                                   driveId: Int)
+
+    func publishFileUploaded(result: UploadCompletionResult)
+}
+
 // MARK: - Publish
 
-extension UploadQueue: UploadPublishable {
+extension UploadService: UploadPublishable {
     func publishUploadCount(withParent parentId: Int,
                             userId: Int,
                             driveId: Int) {
