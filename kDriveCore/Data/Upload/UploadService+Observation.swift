2be00a57823e559bd12a330be8514b00aaae269c
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-03-11 08:53:38 +0100

    feat(UploadService): Observation

--- kDriveCore/Data/Upload/UploadService+Observation.swift
+++ kDriveCore/Data/Upload/UploadService+Observation.swift
@@ -1,6 +1,6 @@
 /*
  Infomaniak kDrive - iOS App
- Copyright (C) 2023 Infomaniak Network SA
+ Copyright (C) 2025 Infomaniak Network SA
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -21,9 +21,47 @@ import Foundation
 import InfomaniakCore
 import RealmSwift
 
+public typealias UploadedFileId = String
+public typealias UploadProgress = Double
+
+public protocol UploadQueueObservable {
+    /// Observe the upload of a file
+    /// - Parameters:
+    ///   - observer: The observer type
+    ///   - fileId: The Id of the file to observe
+    ///   - closure: Called on completion, in MainQueue
+    /// - Returns: A token that should be cancelled first thing in your `closure`
+    @discardableResult
+    func observeFileUploaded<T: AnyObject>(_ observer: T,
+                                           fileId: String?,
+                                           using closure: @escaping (UploadFile, File?) -> Void) -> ObservationToken
+
+    /// Observe Uploaded files within a specific folder.
+    /// - Parameters:
+    ///   - observer: The observer type
+    ///   - parentId: parent Id
+    ///   - closure: Called on completion, in MainQueue
+    /// - Returns: A token that should be cancelled first thing in your `closure`
+    @discardableResult
+    func observeUploadCount<T: AnyObject>(_ observer: T,
+                                          parentId: Int,
+                                          using closure: @escaping (Int, Int) -> Void) -> ObservationToken
+
+    /// Observe the count of uploaded files within a Drive
+    /// - Parameters:
+    ///   - observer: The observer type
+    ///   - driveId: drive Id
+    ///   - closure: Called on completion, in MainQueue
+    /// - Returns: A token that should be cancelled first thing in your `closure`
+    @discardableResult
+    func observeUploadCount<T: AnyObject>(_ observer: T,
+                                          driveId: Int,
+                                          using closure: @escaping (Int, Int) -> Void) -> ObservationToken
+}
+
 // MARK: - Observation
 
-extension UploadQueue: UploadQueueObservable {
+extension UploadService: UploadQueueObservable {
     @discardableResult
     public func observeFileUploaded<T: AnyObject>(_ observer: T,
                                                   fileId: String? = nil,
