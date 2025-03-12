b5121ceef1f24f2e3b325df7d80691915006eed6
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-03-12 16:00:39 +0100

    chore(UploadOperation): Sonar asked to remove explicit self

--- kDriveCore/Data/Upload/UploadQueue/Operation/UploadOperation.swift
+++ kDriveCore/Data/Upload/UploadQueue/Operation/UploadOperation.swift
@@ -250,10 +250,10 @@ public final class UploadOperation: AsynchronousOperation, UploadOperationable {
             Log.uploadOperation("call finish ufid:\(uploadFileId)")
 
             // Make sure we stop the expiring activity
-            self.expiringActivity?.endAll()
+            expiringActivity?.endAll()
 
             // Make sure we stop all the network requests (if any)
-            self.cancelAllUploadRequests()
+            cancelAllUploadRequests()
 
             finish()
 
@@ -449,7 +449,7 @@ public final class UploadOperation: AsynchronousOperation, UploadOperationable {
         }
 
         defer {
-            self.end()
+            end()
         }
 
         if let data {
