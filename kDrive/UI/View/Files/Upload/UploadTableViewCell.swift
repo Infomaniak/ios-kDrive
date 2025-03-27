07e87291e9319c921b0dca346372159854c34477
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-03-27 15:52:54 +0100

    refactor(UploadTableViewCell): Modern if syntax

--- kDrive/UI/View/Files/Upload/UploadTableViewCell.swift
+++ kDrive/UI/View/Files/Upload/UploadTableViewCell.swift
@@ -88,9 +88,9 @@ final class UploadTableViewCell: InsetTableViewCell {
             var status = KDriveResourcesStrings.Localizable.uploadInProgressPending
             if ReachabilityListener.instance.currentStatus == .offline {
                 status = KDriveResourcesStrings.Localizable.uploadNetworkErrorDescription
-            } else if UserDefaults.shared.isWifiOnly
-                && ReachabilityListener.instance.currentStatus != .wifi
-                && uploadFile.isPhotoSyncUpload {
+            } else if UserDefaults.shared.isWifiOnly,
+                      ReachabilityListener.instance.currentStatus != .wifi,
+                      uploadFile.isPhotoSyncUpload {
                 status = KDriveResourcesStrings.Localizable.uploadNetworkErrorWifiRequired
             }
 
