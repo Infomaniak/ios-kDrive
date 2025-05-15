e39a66b28189cac3b59fca5a3a0d6fa272689c93
Baptiste Griva <baptiste.griva@infomaniak.com>
2025-05-15 10:29:51 +0200

    feat: Add feedback row in parameterTableView

--- kDrive/UI/Controller/Menu/ParameterTableViewController.swift
+++ kDrive/UI/Controller/Menu/ParameterTableViewController.swift
@@ -16,6 +16,7 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import InfomaniakBugTracker
 import InfomaniakCoreCommonUI
 import InfomaniakCoreUIKit
 import InfomaniakCoreUIResources
@@ -82,6 +83,7 @@ class ParameterTableViewController: BaseGroupedTableViewController {
         case storage
         case about
         case joinBeta
+        case feedback
         case deleteAccount
 
         var title: String {
@@ -102,14 +104,17 @@ class ParameterTableViewController: BaseGroupedTableViewController {
                 return KDriveResourcesStrings.Localizable.aboutTitle
             case .joinBeta:
                 return CoreUILocalizable.joinTheBetaButton
+            case .feedback:
+                return "Feedback"
             case .deleteAccount:
                 return KDriveResourcesStrings.Localizable.deleteMyAccount
             }
         }
     }
 
-    private let visibleRows: [GeneralParameterRow] = GeneralParameterRow.allCases
+    private lazy var visibleRows: [GeneralParameterRow] = GeneralParameterRow.allCases
         .filter { $0 != .joinBeta || !Bundle.main.isRunningInTestFlight }
+        .filter { $0 != .feedback || accountManager.currentAccount?.user.isStaff == true }
 
     init(driveFileManager: DriveFileManager) {
         self.driveFileManager = driveFileManager
@@ -243,7 +248,7 @@ class ParameterTableViewController: BaseGroupedTableViewController {
             }
             return cell
 
-        case .security, .storage, .about, .joinBeta, .deleteAccount:
+        case .security, .storage, .about, .joinBeta, .feedback, .deleteAccount:
             let cell = tableView.dequeueReusableCell(type: ParameterAboutTableViewCell.self, for: indexPath)
             cell.initWithPositionAndShadow(
                 isFirst: indexPath.row == 0,
@@ -336,6 +341,8 @@ class ParameterTableViewController: BaseGroupedTableViewController {
         case .joinBeta:
             UIApplication.shared.open(URLConstants.testFlight.url)
             matomo.track(eventWithCategory: .settings, name: "joinBetaProgram")
+        case .feedback:
+            present(BugTrackerViewController(), animated: true)
         case .deleteAccount:
             let deleteAccountViewController = DeleteAccountViewController.instantiateInViewController(
                 delegate: self,
