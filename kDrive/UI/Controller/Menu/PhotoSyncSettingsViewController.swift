f620887c4c89c58793e70cfa0e8c80df730babac
Baptiste Griva <baptiste.griva@infomaniak.com>
2025-05-16 13:26:04 +0200

    fix: Remove double var declaration

--- kDrive/UI/Controller/Menu/PhotoSyncSettingsViewController.swift
+++ kDrive/UI/Controller/Menu/PhotoSyncSettingsViewController.swift
@@ -33,6 +33,7 @@ final class PhotoSyncSettingsViewController: BaseGroupedTableViewController {
     @LazyInjectService var photoLibraryUploader: PhotoLibraryUploadable
     @LazyInjectService var freeSpaceService: FreeSpaceService
     @LazyInjectService var uploadService: UploadServiceable
+    @LazyInjectService var uploadDataSource: UploadServiceDataSourceable
 
     private enum PhotoSyncSection: Int {
         case syncSwitch
@@ -233,8 +234,6 @@ final class PhotoSyncSettingsViewController: BaseGroupedTableViewController {
         }
 
         if didDriveSelectionChange {
-            @InjectService var uploadDataSource: UploadServiceDataSourceable
-            @InjectService(customTypeIdentifier: kDriveDBID.uploads) var uploadsDatabase: Transactionable
             let objectsToDelete = uploadDataSource
                 .getUploadedFiles(optionalPredicate: PhotoLibraryCleanerService.photoAssetPredicate)
             try? uploadsDatabase.writeTransaction { writableRealm in
