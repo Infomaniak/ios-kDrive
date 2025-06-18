ea296ff413487823c5867c9f27a701a2fbf9dd80
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-06-18 10:18:06 +0200

    fix(UploadOperation+Error): Make sure we do not try to cancel upload session on remote for a small or empty file where it does not exist

--- kDriveCore/Data/Upload/UploadQueue/Queue/UploadQueue+Queue.swift
+++ kDriveCore/Data/Upload/UploadQueue/Queue/UploadQueue+Queue.swift
@@ -111,7 +111,7 @@ extension UploadQueue: UploadQueueable {
         if let operation = keyedUploadOperations.getObject(forKey: uploadFileId) {
             Log.uploadQueue("\(self) operation to cancel:\(operation)")
             Task {
-                await operation.cleanUploadFileSession()
+                try await operation.cleanUploadFileSession()
                 operation.cancel()
             }
         }
