817ba1502dbe163f2ce943b56366b681ebd4e407
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2025-06-27 15:03:10 +0200

    ci: Retry sentry upload
    Signed-off-by: Philippe Weidmann <philippe.weidmann@infomaniak.com>

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -12,10 +12,21 @@ mise install
 eval "$(mise activate bash --shims)"
 
 
-if [[ -n $CI_ARCHIVE_PATH ]];
-then
-    # Upload dSYMs
-    sentry-cli --url $SENTRY_URL --auth-token $SENTRY_AUTH_TOKEN upload-dif --org sentry --project $SENTRY_PROJECT --include-sources $CI_DERIVED_DATA_PATH
+if [[ -n $CI_ARCHIVE_PATH ]]; then
+    retries=0
+    max_retries=3
+    until [ $retries -ge $max_retries ]
+    do
+        sentry-cli --url $SENTRY_URL --auth-token $SENTRY_AUTH_TOKEN upload-dif --org sentry --project $SENTRY_PROJECT --include-sources $CI_DERIVED_DATA_PATH && break
+        retries=$((retries+1))
+        echo "Retry $retries/$max_retries for sentry-cli failed."
+        sleep 2
+    done
+
+    if [ $retries -eq $max_retries ]; then
+        echo "sentry-cli failed after $max_retries attempts."
+        exit 1
+    fi
 else
     echo "Archive path isn't available. Unable to run dSYMs uploading script."
 fi
