3a8b057e4b435685da955350063707c50828a291
Matthieu Déglon <matthieu.deglon@infomaniak.com>
2025-07-01 06:44:23 +0200

    fix: Position of folders

--- kDrive/UI/Controller/Files/SidebarViewController.swift
+++ kDrive/UI/Controller/Files/SidebarViewController.swift
@@ -376,7 +376,7 @@ class SidebarViewController: CustomLargeTitleCollectionViewController, SelectSwi
             switch menuSection {
             case .recent:
                 guard let cell = collectionView.dequeueReusableCell(
-                    withReuseIdentifier: "FileCollectionViewCell",
+                    withReuseIdentifier: FileCollectionViewCell.identifier,
                     for: indexPath
                 ) as? FileCollectionViewCell else {
                     fatalError("Failed to dequeue cell")
@@ -392,7 +392,7 @@ class SidebarViewController: CustomLargeTitleCollectionViewController, SelectSwi
                     selectionMode: false
                 )
                 cell.configure(with: viewModel)
-                cell.initStyle(isFirst: menuItem.isFirst, isLast: menuItem.isLast)
+                cell.initStyle(isFirst: menuItem.isFirst, isLast: menuItem.isLast, custom: true)
                 cell.setEnabled(true)
                 cell.moreButton.isHidden = true
 
@@ -495,7 +495,8 @@ class SidebarViewController: CustomLargeTitleCollectionViewController, SelectSwi
             }
 
             let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                    heightDimension: .estimated(0))
+                                                    heightDimension: .estimated(8))
+            // margin ? contentInsets
 
             if sectionIndex == 0 && !selectMode {
                 let sectionHeaderItem = NSCollectionLayoutBoundarySupplementaryItem(
