a760ded0960e9d295b3dc808ddfd428b57b61a97
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-07-24 17:44:51 +0200

    chore: Lint a file changed during a rebase

--- kDrive/UI/Controller/LoginDelegateHandler.swift
+++ kDrive/UI/Controller/LoginDelegateHandler.swift
@@ -42,7 +42,7 @@ public final class LoginDelegateHandler: @preconcurrency InfomaniakLoginDelegate
             return .preprod
         }
     }
-  
+
     var didStartLoginCallback: (() -> Void)?
     var didCompleteLoginCallback: (() -> Void)?
     var didFailLoginWithErrorCallback: ((Error) -> Void)?
