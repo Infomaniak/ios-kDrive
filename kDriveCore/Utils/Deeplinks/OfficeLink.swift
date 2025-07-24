d7813dc495d66a2b1a6f23f52c918e5b22747381
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-07-24 17:23:47 +0200

    feat: OfficeLink now equatable

--- kDriveCore/Utils/Deeplinks/OfficeLink.swift
+++ kDriveCore/Utils/Deeplinks/OfficeLink.swift
@@ -19,7 +19,7 @@
 import Foundation
 import SwiftRegex
 
-public struct OfficeLink: Sendable {
+public struct OfficeLink: Sendable, Equatable {
     public static let parsingRegex = Regex(pattern: #"^/app/office/([0-9]+)/([0-9]+)$"#)
 
     public let officeURL: URL
