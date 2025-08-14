3f42f07bd6c754ef22350fed9dfbaf5e4b08cb46
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-08-14 11:08:12 +0200

    feat(KSuiteGetProViewController): New wrapper view controller

---
+++ kDrive/Utils/KSuitePro/KSuiteGetProViewController.swift
@@ -0,0 +1,45 @@
+/*
+ Infomaniak kDrive - iOS App
+ Copyright (C) 2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import kDriveCore
+import kDriveResources
+import KSuite
+import SwiftUI
+import UIKit
+
+class KSuiteGetProViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let kSuiteView = KSuiteGetProView()
+        let hostingController = UIHostingController(rootView: kSuiteView)
+
+        addChild(hostingController)
+        view.addSubview(hostingController.view)
+
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+
+        hostingController.didMove(toParent: self)
+    }
+}
