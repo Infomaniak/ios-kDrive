36d8602df561b3f7424a7f25d4ae970ccb87660f
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-08-15 17:24:23 +0200

    refactor: New factorized generic SwiftUI wrapper

--- kDrive/Utils/KSuitePro/KSuiteProChipController.swift
+++ kDrive/Utils/KSuitePro/KSuiteProChipController.swift
@@ -22,25 +22,13 @@ import KSuite
 import SwiftUI
 import UIKit
 
-class KSuiteProChipController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class KSuiteProChipController: GenericHostingViewController<KSuiteProUpgradeChip> {
+    init() {
+        super.init(contentView: KSuiteProUpgradeChip())
+    }
 
-        let kSuiteView = KSuiteProUpgradeChip()
-        let hostingController = UIHostingController(rootView: kSuiteView)
-
-        addChild(hostingController)
-        view.addSubview(hostingController.view)
-
-        hostingController.view.backgroundColor = .clear
-        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
-            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ])
-
-        hostingController.didMove(toParent: self)
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
