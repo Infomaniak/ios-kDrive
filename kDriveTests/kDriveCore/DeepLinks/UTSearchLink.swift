28e87881c65e324d25e82fe9cdf637c7b67c70b0
Baptiste Griva <baptiste.griva@infomaniak.com>
2025-08-19 10:21:18 +0200

    feat: Add all query attributes in the parser

--- kDriveTests/kDriveCore/DeepLinks/UTSearchLink.swift
+++ kDriveTests/kDriveCore/DeepLinks/UTSearchLink.swift
@@ -23,12 +23,20 @@ import Testing
 @Suite("UTSearchLink")
 struct UTSearchLink {
     @Test(
-        "Parse driveId and query from a search link",
-        arguments: [143_883], ["projet"]
+        "Parse all attributes from a search link",
+        arguments: [(143_883, 1_754_949_600, 1_755_295_199, "searchQueryTest", ConvertedType.image, 6, "or")]
     )
-    func parseSearchLink(driveId: Int, query: String) async throws {
+    func parseSearchLink(
+        driveId: Int,
+        modifiedAfter: Int,
+        modifiedBefore: Int,
+        query: String,
+        type: ConvertedType,
+        categoryId: Int,
+        categoryOperator: String
+    ) async throws {
         let givenLink =
-            "https://ksuite.infomaniak.com/all/kdrive/app/drive/\(driveId)/search?q=%7B%22kind%22%3A%22default%22%2C%22directory_id%22%3A1%2C%22query%22%3A%22\(query)%22%7D"
+            "https://ksuite.infomaniak.com/all/kdrive/app/drive/\(driveId)/search?q=%7B%22modified_at%22%3A%22custom%22%2C%22modified_after%22%3A\(modifiedAfter)%2C%22modified_before%22%3A\(modifiedBefore)%2C%22kind%22%3A%22default%22%2C%22directory_id%22%3A1%2C%22query%22%3A%22\(query)%22%2C%22type%22%3A%22\(type)%22%2C%22category_ids%22%3A%5B\(categoryId)%5D%2C%22category_operator%22%3A%22\(categoryOperator)%22%7D"
 
         guard let url = URL(string: givenLink), let parsedResult = SearchLink(searchURL: url) else {
             Issue.record("Failed to parse the URL")
@@ -37,7 +45,12 @@ struct UTSearchLink {
 
         #expect(parsedResult.searchURL == url)
         #expect(parsedResult.driveId == driveId)
-        #expect(parsedResult.query == query)
+        #expect(parsedResult.modifiedAfter == modifiedAfter)
+        #expect(parsedResult.modifiedBefore == modifiedBefore)
+        #expect(parsedResult.searchQuery == query)
+        #expect(parsedResult.type == type)
+        #expect(parsedResult.categoryIds == [categoryId])
+        #expect(parsedResult.categoryOperator == categoryOperator)
     }
 
     @Test(
