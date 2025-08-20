8755418f5f2cf943eb4e7559579c7d8ff7548cd8
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-08-20 10:58:45 +0200

    feat: New matomo category

--- kDriveCore/Utils/MatomoUtils+Extension.swift
+++ kDriveCore/Utils/MatomoUtils+Extension.swift
@@ -58,6 +58,7 @@ public extension MatomoUtils.EventCategory {
     static let publicSharePasswordAction = MatomoUtils.EventCategory(displayName: "publicSharePasswordAction")
     static let myKSuite = MatomoUtils.EventCategory(displayName: "myKSuite")
     static let myKSuiteUpgradeBottomSheet = MatomoUtils.EventCategory(displayName: "myKSuiteUpgradeBottomSheet")
+    static let KSuiteProUpgradeBottomSheet = MatomoUtils.EventCategory(displayName: "KSuiteProUpgradeBottomSheet")
 }
 
 public extension MatomoUtils {
