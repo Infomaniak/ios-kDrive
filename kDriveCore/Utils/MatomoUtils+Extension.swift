b326dae2a3dec19200d3913ce7ff035ff0b39448
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2025-08-20 15:42:13 +0200

    chore: Renamed kSuiteProUpgradeBottomSheet to begin with a lowercase

--- kDriveCore/Utils/MatomoUtils+Extension.swift
+++ kDriveCore/Utils/MatomoUtils+Extension.swift
@@ -58,7 +58,7 @@ public extension MatomoUtils.EventCategory {
     static let publicSharePasswordAction = MatomoUtils.EventCategory(displayName: "publicSharePasswordAction")
     static let myKSuite = MatomoUtils.EventCategory(displayName: "myKSuite")
     static let myKSuiteUpgradeBottomSheet = MatomoUtils.EventCategory(displayName: "myKSuiteUpgradeBottomSheet")
-    static let KSuiteProUpgradeBottomSheet = MatomoUtils.EventCategory(displayName: "KSuiteProUpgradeBottomSheet")
+    static let kSuiteProUpgradeBottomSheet = MatomoUtils.EventCategory(displayName: "kSuiteProUpgradeBottomSheet")
 }
 
 public extension MatomoUtils {
