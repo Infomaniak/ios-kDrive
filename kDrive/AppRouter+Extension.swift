cd38b36870aa32a6fa06dd39d19b2c28dde4f8e4
Baptiste Griva <baptiste.griva@infomaniak.com>
2025-08-21 15:24:56 +0200

    feat: Create AppRouter extension to handle all generic links

---
+++ kDrive/AppRouter+Extension.swift
@@ -0,0 +1,98 @@
+/*
+ Infomaniak kDrive - iOS App
+ Copyright (C) 2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Foundation
+import InfomaniakDI
+import kDriveCore
+import UIKit
+
+public enum BasicLinkTab: String {
+    case recents
+    case trash
+    case myShares = "my-shares"
+    case favorites
+
+    @MainActor func makeViewModel(driveFileManager: DriveFileManager) -> FileListViewModel {
+        switch self {
+        case .recents:
+            return LastModificationsViewModel(driveFileManager: driveFileManager)
+        case .trash:
+            return TrashListViewModel(driveFileManager: driveFileManager)
+        case .myShares:
+            return MySharesViewModel(driveFileManager: driveFileManager)
+        case .favorites:
+            return FavoritesViewModel(driveFileManager: driveFileManager)
+        }
+    }
+
+    @MainActor func makeViewController(driveFileManager: DriveFileManager) -> UIViewController {
+        let viewModel = makeViewModel(driveFileManager: driveFileManager)
+        return FileListViewController(viewModel: viewModel)
+    }
+}
+
+public extension AppRouter {
+    @MainActor func showBasicTab(
+        driveFileManager: DriveFileManager,
+        viewController: UISplitViewController,
+        basicLink: BasicLink
+    ) async {
+        @LazyInjectService var deeplinkService: DeeplinkServiceable
+
+        defer { deeplinkService.clearLastPublicShare() }
+
+        guard let navigationController =
+            getCurrentController(
+                tabBarViewController: viewController
+            ) as? UINavigationController
+        else {
+            return
+        }
+
+        guard let fileId = basicLink.fileId else {
+            if let tab = BasicLinkTab(rawValue: basicLink.destination) {
+                let fileListViewController = tab.makeViewController(driveFileManager: driveFileManager)
+                navigationController.pushViewController(fileListViewController, animated: true)
+            }
+            return
+        }
+
+        await handleSimpleLink(deeplink: basicLink, fileId: fileId, isOfficeLink: false)
+    }
+
+    @MainActor func handleBasicLink(basicLink: BasicLink) async {
+        guard let driveFileManager = await accountManager
+            .getMatchingDriveFileManagerOrSwitchAccount(deeplink: basicLink) else {
+            Log.sceneDelegate(
+                "NavigationManager: Unable to navigate to a tab without a DriveFileManager",
+                level: .error
+            )
+            deeplinkService.setLastPublicShare(basicLink)
+            return
+        }
+
+        let freshRootViewController = RootSplitViewController(driveFileManager: driveFileManager, selectedIndex: 1)
+        window?.rootViewController = freshRootViewController
+
+        await showBasicTab(
+            driveFileManager: driveFileManager,
+            viewController: freshRootViewController,
+            basicLink: basicLink
+        )
+    }
+}
