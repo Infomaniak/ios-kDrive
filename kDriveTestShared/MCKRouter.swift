0f0c8582aa67c238cddbdfdcf81a9219daed865f
Baptiste Griva <baptiste.griva@infomaniak.com>
2025-11-20 15:33:46 +0100

    feat: Get token from the password view

--- kDriveTestShared/MCKRouter.swift
+++ kDriveTestShared/MCKRouter.swift
@@ -180,7 +180,7 @@ public final class MCKRouter: AppNavigable {
         logNoop()
     }
 
-    @MainActor public func presentPublicShareLocked(_ destinationURL: URL) {
+    @MainActor public func presentPublicShareLocked(_ link: PublicShareLink) {
         logNoop()
     }
 
