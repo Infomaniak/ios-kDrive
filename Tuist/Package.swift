97b2f96c2ec80e8fc46450d71899cc68c0c59745
Baptiste Griva <baptiste.griva@infomaniak.com>
2026-02-25 11:54:46 +0100

    chore: Bump lib features

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -82,7 +82,7 @@ let package = Package(
         .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "24.5.0")),
         .package(url: "https://github.com/Infomaniak/ios-dependency-injection", .upToNextMajor(from: "2.0.5")),
         .package(url: "https://github.com/Infomaniak/ios-device-check", .upToNextMajor(from: "1.1.1")),
-        .package(url: "https://github.com/Infomaniak/ios-features", .upToNextMajor(from: "8.5.0")),
+        .package(url: "https://github.com/Infomaniak/ios-features", .upToNextMajor(from: "8.6.0")),
         .package(url: "https://github.com/Infomaniak/ios-login", .upToNextMajor(from: "7.2.0")),
         .package(url: "https://github.com/Infomaniak/ios-notifications", .upToNextMajor(from: "15.1.0")),
         .package(url: "https://github.com/Infomaniak/ios-onboarding", .upToNextMajor(from: "1.2.3")),
