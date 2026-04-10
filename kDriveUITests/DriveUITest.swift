78514bf7ea3327b7c1acf437d7095e0b84c26a4f
Killian Mathias <killian.mathias@infomaniak.com>
2026-04-10 11:44:54 +0200

    fix: Correction of PR Review

--- kDriveUITests/DriveUITest.swift
+++ kDriveUITests/DriveUITest.swift
@@ -51,8 +51,8 @@ class AppUITest: XCTestCase {
             app.launchArguments += ["resetData"]
         }
         app.launchArguments += ["testing"]
-        app.launchArguments += ["-AppleLanguages", "(fr)"]
-        app.launchArguments += ["-AppleLocale", "fr_FR"]
+        app.launchArguments += ["-AppleLanguages", "(en)"]
+        app.launchArguments += ["-AppleLocale", "en_UK"]
         app.launch()
     }
 
@@ -143,9 +143,8 @@ class AppUITest: XCTestCase {
             }
         }
         let photospickerApp = XCUIApplication(bundleIdentifier: "com.apple.mobileslideshow.photospicker")
-        photospickerApp/*@START_MENU_TOKEN@*/
-            .images["Photo, 30 mars 2018, 21:14"]/*[[".otherElements.images[\"Photo, 30 mars 2018, 21:14\"]",".images[\"Photo, 30 mars 2018, 21:14\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            .coordinate(withNormalizedOffset: CGVector()).tap()
+        XCTAssertTrue(photospickerApp.images.debugDescription.isEmpty, "No photos in photo library")
+        photospickerApp.images.element(boundBy: 1).coordinate(withNormalizedOffset: CGVector(dx: 0.5,dy: 0.5)).tap()
         photospickerApp/*@START_MENU_TOKEN@*/
             .buttons["Add"]/*[[".navigationBars",".buttons[\"Terminé\"]",".buttons[\"Add\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
             .firstMatch.tap()
@@ -562,7 +561,7 @@ class AppUITest: XCTestCase {
         tablesQuery.staticTexts[KDriveResourcesStrings.Localizable.addCategoriesTitle].tap()
         tablesQuery.cells.firstMatch.tap()
         app/*@START_MENU_TOKEN@*/
-            .buttons["BackButton"]/*[[".navigationBars",".buttons[\"Filtres\"]",".buttons[\"BackButton\"]",".buttons"],[[[-1,2],[-1,1],[-1,3],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+            .buttons[KDriveResourcesStrings.Localizable.buttonBack]/*[[".navigationBars",".buttons[\"Filtres\"]",".buttons[\"BackButton\"]",".buttons"],[[[-1,2],[-1,1],[-1,3],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
             .firstMatch.tap()
         app.staticTexts[KDriveResourcesStrings.Localizable.buttonApplyFilters].firstMatch.tap()
         XCTAssertTrue(app.staticTexts[root].waitForExistence(timeout: 4), "Directory with category should be in result")
