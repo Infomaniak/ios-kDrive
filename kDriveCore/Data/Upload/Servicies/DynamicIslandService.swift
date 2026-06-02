4d4a714156806fdf7b9c36209780e862d8ce3917
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-06-02 09:24:12 +0200

    feat: Add function to register and create Task for Dynamic Island + Singleton

--- kDriveCore/Data/Upload/Servicies/DynamicIslandService.swift
+++ kDriveCore/Data/Upload/Servicies/DynamicIslandService.swift
@@ -16,16 +16,87 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import BackgroundTasks
+@preconcurrency import Combine
 import Foundation
+import InfomaniakDI
+import kDriveResources
 
-struct DynamicIslandService {
+@available(iOS 26.0, *)
+public struct DynamicIslandService {
+    public static let shared = DynamicIslandService()
+
+    @LazyInjectService private var dynamicIslandManager: DynamicIslandManager
+    @LazyInjectService private var uploadService: UploadServiceable
+
     private enum DomainError: Error {
         case expiredTask
     }
 
     private let taskIdentifier: String
 
-    init() {
+    private init() {
         taskIdentifier = "com.infomaniak.drive.background-upload-dynamic-island.\(UUID().uuidString)"
+    }
+
+    public func registerTask() {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: taskIdentifier, using: nil) { task in
+            guard let task = task as? BGContinuedProcessingTask else { return }
+            self.handle(task: task)
+        }
+
+        dynamicIslandManager.setup()
+    }
+
+    public func submitTask() {
+        // TODO: Strings are hard-coded, use string resources with Loco
+        let request = BGContinuedProcessingTaskRequest(
+            identifier: taskIdentifier,
+            title: "Uploading",
+            subtitle: "Preparation..."
+        )
+        request.strategy = .fail
+
+        do {
+            try BGTaskScheduler.shared.submit(request)
+            print("Dynamic Island task submitted")
+        } catch {
+            print("Error submitting task : \(error)")
+        }
+    }
+
+    private func handle(task: BGContinuedProcessingTask) {
+        task.expirationHandler = {
+            task.setTaskCompleted(success: false)
+        }
+
+        Task {
+            var cancellable: AnyCancellable?
+            task.progress.totalUnitCount = 100
+
+            // TODO: Check the value of progress
+            cancellable = dynamicIslandManager.$fractionCompleted.sink { progress in
+                task.progress.completedUnitCount = Int64(progress * 100)
+                task.updateTitle(
+                    KDriveResourcesStrings.Localizable.uploadInProgressTitle,
+                    subtitle: "Progress \(progress.formatted(.defaultPercent))"
+                )
+            }
+
+            await withCheckedContinuation { uploadContinuation in
+                dynamicIslandManager.uploadService.waitForCompletion {
+                    uploadContinuation.resume()
+                }
+            }
+
+            task.setTaskCompleted(success: true)
+            cancellable?.cancel()
+        }
+    }
+}
+
+public extension FormatStyle where Self == FloatingPointFormatStyle<Double>.Percent {
+    static var defaultPercent: FloatingPointFormatStyle<Double>.Percent {
+        return .percent.precision(.fractionLength(0))
     }
 }
