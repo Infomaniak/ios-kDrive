85b62b2a2c7752be6dd73a72a143e527f973132c
Ambroise Decouttere <ambroise.decouttere@infomaniak.com>
2026-06-03 10:53:59 +0200

    fix: Disable only directory

--- kDrive/Utils/FileActionsHelper.swift
+++ kDrive/Utils/FileActionsHelper.swift
@@ -213,7 +213,7 @@ public final class FileActionsHelper {
                             presentViewController: (UIViewController) -> Void,
                             completion: (() -> Void)? = nil) {
         // Current directory is always disabled.
-        var disabledDirectoriesIds = [currentDirectory.id] + files.map(\.id)
+        var disabledDirectoriesIds = [currentDirectory.id] + files.compactMap { $0.isDirectory ? $0.id : nil }
         if let firstSelectedParentId = files.first?.parentId,
            firstSelectedParentId != currentDirectory.id,
            files.allSatisfy({ $0.parentId == firstSelectedParentId }) {
