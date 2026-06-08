eab84f36d3494f46c7fefe524bc28b6575b53a60
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-06-08 16:07:44 +0200

    fix: Upload number mistake if the last uploading file uses chunks

--- kDriveCore/Data/Upload/DynamicIslandManager.swift
+++ kDriveCore/Data/Upload/DynamicIslandManager.swift
@@ -186,6 +186,10 @@ final class DynamicIslandManager: ObservableObject {
         return progressUploading
     }
 
+    func lastIsChunck() -> Bool {
+        return progessChunckUploading == 1
+    }
+
     func reset() {
         totalUploadCount = 0
         progressUploading = 0
