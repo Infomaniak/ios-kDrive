1820ee38de68aefdb52673839fde1742b4373b12
Killian Mathias <killian.mathias@infomaniak.com>
2026-06-10 10:26:22 +0200

    chore: Update features version

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -64,7 +64,7 @@ let package = Package(
         .package(url: "https://github.com/Infomaniak/ios-core-ui", .upToNextMajor(from: "24.12.0")),
         .package(url: "https://github.com/Infomaniak/ios-dependency-injection", .upToNextMajor(from: "2.0.5")),
         .package(url: "https://github.com/Infomaniak/ios-device-check", .upToNextMajor(from: "1.1.1")),
-        .package(url: "https://github.com/Infomaniak/ios-features", branch: "fix/app-lock-construction"),
+        .package(url: "https://github.com/Infomaniak/ios-features", .upToNextMajor(from: "8.11.0")),
         .package(url: "https://github.com/Infomaniak/ios-login", .upToNextMajor(from: "7.2.0")),
         .package(url: "https://github.com/Infomaniak/ios-notifications", .upToNextMajor(from: "15.1.0")),
         .package(url: "https://github.com/Infomaniak/ios-onboarding", .upToNextMajor(from: "1.2.3")),
