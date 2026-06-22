cb4de4e385778751fb691b327a839554e7f3c9c9
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-06-22 11:44:46 +0200

    fix: Revet `operationDidCancel`
    The end method is already called by another method

--- kDriveCore/Data/Upload/UploadQueue/Operation/UploadOperation.swift
+++ kDriveCore/Data/Upload/UploadQueue/Operation/UploadOperation.swift
@@ -324,10 +324,6 @@ public final class UploadOperation: AsynchronousOperation, UploadOperationable, 
         }
     }
 
-    override public func operationDidCancel() {
-        end()
-    }
-
     // MARK: - Private methods -
 
     // MARK: Progress
