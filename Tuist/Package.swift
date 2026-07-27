e656b4e774ef61086a2a9d18c798b7b611600f98
GRANDY Kylian <kylian.grandy@infomaniak.com>
2026-07-27 13:54:45 +0200

    feat: Use `shouldUseWithAccounts` if debug and prod
    # Conflicts:
    #	Tuist/Package.resolved

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -66,7 +66,7 @@ let package = Package(
         .package(url: "https://github.com/Infomaniak/ios-core-uikit", .upToNextMajor(from: "2.0.1")),
         .package(url: "https://github.com/Infomaniak/ios-dependency-injection", .upToNextMajor(from: "2.0.5")),
         .package(url: "https://github.com/Infomaniak/ios-device-check", .upToNextMajor(from: "1.1.1")),
-        .package(url: "https://github.com/Infomaniak/ios-features", .upToNextMajor(from: "10.1.2")),
+        .package(url: "https://github.com/Infomaniak/ios-features", branch: "feat/Add-parameter-to-not-use-withAccounts"),
         .package(url: "https://github.com/Infomaniak/ios-login", .upToNextMajor(from: "7.2.0")),
         .package(url: "https://github.com/Infomaniak/ios-notifications", .upToNextMajor(from: "16.0.0")),
         .package(url: "https://github.com/Infomaniak/ios-onboarding", .upToNextMajor(from: "1.2.3")),
