90069be515edddc6e2ff551129d280931eedb6d6
Baptiste Griva <baptiste.griva@infomaniak.com>
2026-09-02 17:44:56 +0200

    feat: Add an App Entity

---
+++ kDrive/App Intents/KDriveExtensionAppIntentsPackage.swift
@@ -0,0 +1,27 @@
+/*
+ Infomaniak kDrive - iOS App
+ Copyright (C) 2025 Infomaniak Network SA
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import AppIntents
+import kDriveCore
+
+@available(iOS 17.0, *)
+struct KDriveAppIntentsPackage: AppIntentsPackage {
+    static var includedPackages: [any AppIntentsPackage.Type] {
+        [KDriveCoreIntentsPackage.self]
+    }
+}
