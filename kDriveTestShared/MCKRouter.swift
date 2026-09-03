e1e9d6c59f216c04f8c54d6f259fa8207b52b892
Baptiste Griva <baptiste.griva@infomaniak.com>
2026-09-03 16:42:44 +0200

    chore: SwiftFormat

--- kDriveTestShared/MCKRouter.swift
+++ kDriveTestShared/MCKRouter.swift
@@ -43,6 +43,10 @@ public final class MCKRouter: AppNavigable {
         logNoop()
     }
 
+    public func openFile(_ target: kDriveCore.FileNavigationTarget) async {
+        logNoop()
+    }
+
     public func askForReview() async {
         logNoop()
     }
